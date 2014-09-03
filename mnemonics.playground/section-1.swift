795fdfee449f4b1dcdd53c109155806020eba584
Charles Vestal <charlesv@gmail.com>
2014-09-03 18:03:00 -0500

    works for exchange rates of less than 1

--- mnemonics.playground/section-1.swift
+++ mnemonics.playground/section-1.swift
@@ -21,15 +21,15 @@ extension Double {
     }
 }
 
-
-
 var homeRate:Double = 1 // dollars per dollar
 var awayRate:Double = 6 // kroner per dollar
 
-func exchangeRate () -> Double {
+func exchangeRate() -> Double {
     var exchangeRate = homeRate / awayRate
     return exchangeRate
 }
+
+
 
 var exchangeString = String(format: "%0.20f", exchangeRate())
 
@@ -60,30 +60,34 @@ else
 }
 
 magnitude = pow(10.0,(magnitude-2.0))
-
 var exchangeRateInt:Int = Int(exchangeRate())
 var magnitudeInt:Int = Int(magnitude)
 
-var roundedExchange:Float = 0.0
 
-
-let exchangeRateValue = exchangeRate()
-
+func roundedExchangeRate(Double) -> Double
+{
 if (exchangeRate() < 1){
-    var myString = String(firstSignificantDigitPositionAfterDecimal)
-    var myDouble = exchangeRate()
-    var roundedExchange = myDouble.format(myString)
-    
-}
+        var myString = String(firstSignificantDigitPositionAfterDecimal)
+        var myDouble = exchangeRate()
+        var roundedExchangeString = NSString(string: myDouble.format(myString))
+        return roundedExchangeString.doubleValue //return
+    }
 else{
-    var myIntValue:Int = Int(exchangeRate())
-    //need to figure out types then i can get the largest multiplier
-    var roundedExchange = (magnitudeInt*((exchangeRateInt + (magnitudeInt/2))/magnitudeInt))
+        var myIntValue = exchangeRate()
+        var roundedExchange = (magnitudeInt*((exchangeRateInt + (magnitudeInt/2))/magnitudeInt))
+        return Double(roundedExchange)
+    }
 }
 
+var multiplyBy: Int = Int(roundedExchangeRate(exchangeRate()) * pow(10.0, Double(firstSignificantDigitPositionAfterDecimal)))
+var variance =  (1 - (exchangeRate() / roundedExchangeRate(exchangeRate()))) * 100
 
+var varianceString = String(format:"%0.f", variance)
 
-
+println("The easiest exchange rate is going to be \(roundedExchangeRate(exchangeRate()))")
+println("So multiply by \(multiplyBy)")
+println("then move the decimal over \(firstSignificantDigitPositionAfterDecimal) places")
+println("(It's actually about \(varianceString) percent less, so think about it as a little less back home in your head)")
 
 
 
