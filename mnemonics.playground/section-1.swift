3f6d33e6705f9b3ad857c176155cd8cafd23bbae
Charles Vestal <charlesv@gmail.com>
2014-09-08 18:52:38 -0500

    always print the multiplier

--- mnemonics.playground/section-1.swift
+++ mnemonics.playground/section-1.swift
@@ -3,8 +3,8 @@
 import UIKit
 
 var homeRate:Double = 1 // home currency to USD
-var awayRate:Double = 0.017 // foreign currency to USD
-var amountToExchange = 96.0
+var awayRate:Double = 21.41 // USD to foreign
+var amountToExchange = 113.0
 
 extension Double {
     func format(f: String) -> String {
@@ -63,10 +63,8 @@ println("at an actual exchange rate of \(exchangeRateValue)")
 
 println("The easiest exchange rate is going to be \(roundedExchange)")
 
-if(multiplyBy != 1)
-{
-    println("So multiply times \(multiplyBy), i.e. \(amountToExchange) x \(multiplyBy) = \(fakeEchangeMultiplied)")
-}
+println("So multiply times \(multiplyBy), i.e. \(amountToExchange) x \(multiplyBy) = \(fakeEchangeMultiplied),")
+
 if (magnitude==0)
 {
     // you're multiplying times a single digit
@@ -78,12 +76,13 @@ else {
    println("and move the decimal \(abs(magnitude)) places to the left, i.e. \(fakeExchangeMovetheDecimal)")
 }
 
+
 if (variance > 0.0) {
-    println("but the actual total is \(actualTotal), so the exchange is really about \(varianceString)% less than that,")
+    println("but the actual total is \(actualTotal), so the real total is about \(varianceString)% less than that,")
     println("So think about everything as that much less than \(roundedExchange)x")
 }
 else
 {
-    println("but the actual exchange is about \(varianceString)% more than that")
+    println("but the actual total is \(actualTotal), so the real total is about \(varianceString)% more than that")
     println("So think about everything as that much more than \(roundedExchange)x ")
 }
